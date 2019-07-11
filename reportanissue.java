6c6e86391d9b2321a35124c99d026f6a949e7cf0
N-C-M <45137335+N-C-M@users.noreply.github.com>
2019-07-11 20:55:20 +0530

    Create reportanissue.java

---
+++ reportanissue.java
@@ -0,0 +1,14 @@
+package app.helloworld.cleanerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reportanissue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportanissue);
+    }
+}
